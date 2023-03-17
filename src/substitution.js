9f6995023ac91aa2722ababe994e40db3639e3e1
Ashley Nguyen <ashleynguyen37@gmail.com>
2023-03-17 15:08:50 -0500

    new commit

---
+++ src/substitution.js
@@ -0,0 +1,50 @@
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
+
+const substitutionModule = (function () {
+  // you can add any code you want within this function scope
+
+  function substitution(input, alphabet, encode = true) {
+    if (!alphabet || alphabet.length !== 26) return false;
+    
+    const alphabetArray = alphabet.split('')
+    
+    const filteredArray = alphabetArray.filter((letter, index)=> alphabetArray.indexOf(letter) !== index)
+    
+    if (filteredArray.length > 0) return false
+
+    const lowerCase = input.toLowerCase();
+
+    const trueAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    const encodedInput = [];
+
+    for (let i = 0; i < lowerCase.length; i++){
+      if (lowerCase[i] === " "){
+        encodedInput.push(" ")
+      }
+
+      for (let j = 0; j < trueAlphabet.length; j++){
+        if (encode === true && trueAlphabet[j] === lowerCase[i]){
+          encodedInput.push(alphabet[j])
+        }
+      } 
+      
+      for (let j = 0; j < alphabet.length; j++){
+        if (encode === false && alphabet[j] === lowerCase[i]){
+          encodedInput.push(trueAlphabet[j])
+        }
+      }
+    };
+
+    return encodedInput.join('');
+  }
+
+  return {
+    substitution,
+  };
+})();
+
+module.exports = { substitution: substitutionModule.substitution };
